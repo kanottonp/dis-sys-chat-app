77b82e1ad58b040e1bcf63686c0420e5fa1ecebe
kanottonp <kanottonp@gmail.com>
2018-05-04 02:41:01 +0700

    Login Completed

--- chat-app-server/models/message.js
+++ chat-app-server/models/message.js
@@ -3,25 +3,13 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var timestamps = require('mongoose-timestamp');
 
-<<<<<<< HEAD
-var connection = mongoose.createConnection("mongodb://localhost:27017/boobooline");
-autoIncrement.initialize(connection);
 
 // create a schema
 var messageSchema = new Schema({
     user: { type: Schema.Types.ObjectId, ref: 'User' },
     text: { type: String, required: true }
-}, { collection: "messages" });
-
-=======
-
-// create a schema
-var messageSchema = new Schema({
-  user: { type: Schema.Types.ObjectId, ref: 'User' },
-  text: { type: String, required: true }
-}, { collection: "messages", timestamps: { createdAt: 'created_at' }});
+}, { collection: "messages", timestamps: { createdAt: 'created_at' } });
 /*
->>>>>>> b926b007c5f8940f71eb45735cdc0e571bca179b
 messageSchema.plugin(autoIncrement.plugin, {
     model: 'Message',
     field: 'messageId'

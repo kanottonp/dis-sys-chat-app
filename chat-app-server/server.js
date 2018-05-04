eacf64b11cde6bfa2aa6fc372914bc031a8fbcbf
Tan <poringz@outlook.com>
2018-05-04 11:31:10 +0700

    just ignore this

--- chat-app-server/server.js
+++ chat-app-server/server.js
@@ -1,10 +1,9 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const morgan = require('morgan')
-const cors = require('cors')
-const port = process.env.PORT || 3333
-
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const cors = require('cors');
+const port = process.env.PORT || 3333;
 
 var Message = require('./models/message.js');
 var Group = require('./models/group.js');
@@ -16,7 +15,7 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/boobooline')
 
 
-const app = express()
+const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -30,40 +29,48 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + '/public/index.html');
 });
 
+app.get('/landing', function(req, res) {
+    res.sendFile(__dirname + '/public/login.html');
+});
+
 app.post('/login', function(req, res) {
+	console.log('login page');
+	console.log(req.body);
+    var username = req.body.username;
 
-    var username = req.body.username
-
-    var newUser = new User()
-    newUser.username = username
-
+    var newUser = new User();
+    newUser.username = username;
+	
+	console.log(username);
     User.findOne({ 'username': newUser.username }, (err, result) => {
-        console.log('hello1')
+        console.log('hello1');
         if (!err) {
-            console.log('hello2')
+            console.log('hello2');
                 // If the document doesn't exist
             if (!result) {
-                console.log('hello3')
+                console.log('hello3');
                     // Create and Save it
                 newUser.save(function(err) {
                     if (err) {
-                        console.log(err)
-                        res.status(400).send()
+                        console.log(err);
+                        res.status(400).send();
                     } else {
-                        res.status(200).send(`${newUser.username} is Registered `)
+                        res.status(200).send(`${newUser.username} is Registered `);
                     }
                 })
             } else {
-                res.status(200).send(`${newUser.username} is logging in `)
+                res.status(200).send(`${newUser.username} is logging in `);
             }
 
         } else {
-            console.log(err)
-            res.status(500).send()
+            console.log(err);
+            res.status(500).send();
         }
-    })
+    });
+	
+	res.redirect('');
 
-})
+});
 
 io.on('connection', function(socket) {
     console.log("a user connected");
